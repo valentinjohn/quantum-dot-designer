9a686c9987965a7e3f61c741636b9854061ca68d
Valentin John <V.John@tudelft.nl>
2023-09-13 13:33:00 +0200

    Put base features in seperate folder

---
+++ base/Element.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 11:07:21 2023
+
+@author: vjohn
+"""
+
+# %% imports
+
+from abc import abstractmethod
+from base import PlotMixin
+
+# %% definition
+
+
+class Element(PlotMixin):
+    def __init__(self, name):
+        """
+        Initialize an Element object.
+
+        Args:
+            name (str): Name of the element.
+            layer (int): Description of layer. Default is None.
+            rotate (float): Rotation of the element. Default is 0.0.
+            fillet (float): Fillet value. Indicates how much the polygon is rounded.
+        """
+
+        self.name = name
+        self.layer = None
+        self.elements = {name: {'vertices': [],
+                                'positions': [],
+                                'layer': self.layer}}
+        self.cell = None
+        self.rotate = 0.0
+        self.fillet = 0.0
+        self.fillet_tolerance = 1e-3
+        self._built = False
+
+    # default attributes to skip for Element
+    _skip_copy_attrs = {'name', 'cell', 'elements'}
+
+    @property
+    def built(self):
+        return self._built
+
+    # This setter is private and can only be used within the class
+    def _set_built(self, new_value: bool):
+        self._built = new_value
+
+    def copy(self, copy_name):
+        # Use the same class as the current instance
+        copied_obj = self.__class__(copy_name)
+
+        for attr, value in self.__dict__.items():
+            if attr not in self._skip_copy_attrs:
+                setattr(copied_obj, attr, value)
+
+        return copied_obj
+
+    @abstractmethod
+    def build(self):
+        """
+        Build the element.
+
+        This method should be implemented in subclasses to build the specific element.
+        """
+        pass
---
+++ base/PlotMixin.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 11:08:32 2023
+
+@author: vjohn
+"""
+# %% imports
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from helpers.helpers import apply_sublattice
+
+from abc import ABC
+
+# %% definition
+
+
+class PlotMixin(ABC):
+    def plot(self, ax=None):
+        # If no axis is provided, create one
+        if ax is None:
+            fig, ax = plt.subplots()
+
+        # Collect unique layers
+        unique_layers = list({el['layer'] for el in self.elements.values()})
+
+        # Create a colormap with enough colors for each layer
+        colors = plt.cm.jet(np.linspace(0, 1, len(unique_layers)))
+
+        # Create a dictionary to map layer values to colors
+        layer_to_color = dict(zip(unique_layers, colors))
+
+        all_x_positions = []  # Collect all x positions to set the x-axis limits
+        all_y_positions = []  # Collect all y positions to set the y-axis limits
+
+        max_polygon_size = 0  # To store the maximum size of the polygon
+
+        # Plot each element
+        for _, el in self.elements.items():
+            vertices = el['vertices']
+            positions = el['positions']
+
+            combined_vertices = apply_sublattice(positions, vertices)
+
+            # Calculate the size of the polygon
+            x_positions, y_positions = zip(*vertices)
+            polygon_width = max(x_positions) - min(x_positions)
+            polygon_height = max(y_positions) - min(y_positions)
+            polygon_size = max(polygon_width, polygon_height)
+            max_polygon_size = max(max_polygon_size, polygon_size)
+
+            for i in range(0, len(combined_vertices), len(vertices)):
+                polygon_vertices = combined_vertices[i:i+len(vertices)]
+                polygon = plt.Polygon(
+                    polygon_vertices, color=layer_to_color[el['layer']])
+                ax.add_patch(polygon)
+
+                # Extract x and y coordinates for axes limits
+                x_positions, y_positions = zip(*polygon_vertices)
+                all_x_positions.extend(x_positions)
+                all_y_positions.extend(y_positions)
+
+        # Set the axes limits with the maximum size of the polygon as margin
+        ax.set_xlim(min(all_x_positions) - max_polygon_size,
+                    max(all_x_positions) + max_polygon_size)
+        ax.set_ylim(min(all_y_positions) - max_polygon_size,
+                    max(all_y_positions) + max_polygon_size)
+
+        ax.set_xlabel(r'width ($\mathrm{\mu m}$)')
+        ax.set_ylabel(r'height ($\mathrm{\mu m}$)')
+        ax.set_title(self.name)
+        ax.set_aspect('equal', adjustable='box')
+
+        if ax is None:
+            plt.show()
